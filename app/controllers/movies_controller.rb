4b5b244f9e4b5c2c772fb4fd76dc921c66abb66e
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-05-31 09:57:35 +0200

    Restricted movie creation, update and deletion to authenticated users.
        https://github.com/plataformatec/devise/wiki/How-To:-Define-resource-actions-that-require-authentication-using-routes.rb
    
      	modified:   app/controllers/movies_controller.rb
      	modified:   config/routes.rb
    
        modified:   yarn.lock

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+  # https://github.com/plataformatec/devise/wiki/How-To:-Define-resource-actions-that-require-authentication-using-routes.rb
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
+  before_action :authenticate_user!, :except => [:show, :index]
 
   # GET /movies
   # GET /movies.json
@@ -62,13 +65,13 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def movie_params
-      params.require(:movie).permit(:title, :txt)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def movie_params
+    params.require(:movie).permit(:title, :txt)
+  end
 end
