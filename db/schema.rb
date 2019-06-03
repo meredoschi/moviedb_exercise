02a6d76ea031848409b6915065e8690cbd469878
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-03 11:39:19 +0200

    Set homepage to categories (index view)
      	modified:   config/routes.rb
      	modified:   db/schema.rb
    
      	new file:   doc/scratch_pad/routes.rb
      	new file:   doc/scratch_pad/routes.rb_previous

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_072524) do
+ActiveRecord::Schema.define(version: 2019_06_03_095103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
