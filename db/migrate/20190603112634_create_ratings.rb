83a780dfd5626204527b3c92737927124daf40a3
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-03 11:39:19 +0200

    rake db:migrate
      	modified:   db/migrate/20190603112634_create_ratings.rb
      	modified:   db/schema.rb

--- db/migrate/20190603112634_create_ratings.rb
+++ db/migrate/20190603112634_create_ratings.rb
@@ -1,10 +1,9 @@
 class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
-      t.integer :user_id
-      t.integer :movie_id
-      t.integer :stars
-
+      t.integer :user_id, null: false
+      t.integer :movie_id, null: false
+      t.integer :stars, default: 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_103051) do
+ActiveRecord::Schema.define(version: 2019_06_03_112634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2019_06_03_103051) do
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
     t.text "txt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "movie_id", null: false
+    t.integer "stars", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
