b1ff9bfda7d8ec125e473d96a5e6d9c426d57f3c
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-05 17:13:19 +0200

    Streamlined contents  	modified:   app/javascript/app.vue  	modified:   app/javascript/packs/hello_vue.js
      modified:   app/views/categories/index.html.erb
     	modified:   public/packs/manifest.json

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,13 +1,11 @@
- import Vue from 'vue/dist/vue.esm'
- import App from '../app.vue'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
 
- document.addEventListener('DOMContentLoaded', () => {
-   const app = new Vue({
-     el: '#hello',
-
-     data: {
-       message: "Can you say hello?"
-     },
-     components: { App }
-   })
- })
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#hello',
+    data: {
+    },
+    components: { App }
+  })
+})
