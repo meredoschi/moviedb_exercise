204268117e1dbf9fa978a64e3aa758403861dcab
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-06 19:23:32 +0200

    modified:   app/javascript/packs/rate.js
      getmovies
      To do: implement removemovie
    
     	modified:   package.json
     	new file:   public/packs/js/rate-29965c0433cac4b6cc92.js
     	new file:   public/packs/js/rate-29965c0433cac4b6cc92.js.map
     	new file:   public/packs/js/rate-2eb62d0f5372fa80d34d.js
     	new file:   public/packs/js/rate-2eb62d0f5372fa80d34d.js.map
     	new file:   public/packs/js/rate-5e6aec8327c5945ba30d.js
     	new file:   public/packs/js/rate-5e6aec8327c5945ba30d.js.map
     	new file:   public/packs/js/rate-7babd35cadcb5e8c0783.js
     	new file:   public/packs/js/rate-7babd35cadcb5e8c0783.js.map
     	new file:   public/packs/js/rate-9240647684940f87cb8c.js
     	new file:   public/packs/js/rate-9240647684940f87cb8c.js.map
     	new file:   public/packs/js/rate-a6b805b94d4b21bb0673.js
     	new file:   public/packs/js/rate-a6b805b94d4b21bb0673.js.map
     	new file:   public/packs/js/rate-b3436cbb6f8cd545ef8a.js
     	new file:   public/packs/js/rate-b3436cbb6f8cd545ef8a.js.map
     	new file:   public/packs/js/rate-f7bc7a84d50bfbe6e4dc.js
     	new file:   public/packs/js/rate-f7bc7a84d50bfbe6e4dc.js.map
     	modified:   public/packs/manifest.json
     	modified:   yarn.lock

--- app/javascript/packs/rate.js
+++ app/javascript/packs/rate.js
@@ -1,33 +1,52 @@
 import Vue from 'vue/dist/vue.esm'
 import Rate from '../rate.vue'
-import axios from 'axios';
+//import axios from 'axios';
+import axios from 'axios-on-rails';
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#rate',
-    data: {
-      errors: [],
-      movies_jsn: []
+    data (){
+
+      return{
+        movies_jsn:[],
+        errors: []
+      }
+
     },
     // https://alligator.io/vuejs/rest-api-axios/
+    // https://stackoverflow.com/questions/54757510/how-to-delete-a-record-in-rails-api-and-vue-js
     created() {
-      axios.get(`/movies.json`).then(response => {
-        this.movies_jsn = response.data
-      }).catch(e => {
-        this.errors.push(e)
-      })
+      this.getmovies()
     },
-    // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
-    components: { Rate }
-    //    ,
-    // https://stackoverflow.com/questions/44440708/vue-js-templating-and-interpolation-not-rendering-data
-    //    delimiters: ["((","))"]
-  })
+    methods: {
+      getmovies () {
+        let url=`/movies.json`;
+        axios.get(url).then(response => {this.movies_jsn = response.data })
+        .catch(e => {this.errors.push(e)})
+        
+//      },
+//      removemovie(id) {
+//        let url=`http://localhost:3000/movies/` + id;
+//        console.log(url)
+//        this.axios.delete(url).then(response => {
+//          this.movies_json = this.movies_json.filter(movie => movie.id !== id)})
+//          .catch(e => {this.errors.push(e)});
+//          console.log(response)
+        }
+
+      },
+      // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
+      components: { Rate }
+      //    ,
+      // https://stackoverflow.com/questions/44440708/vue-js-templating-and-interpolation-not-rendering-data
+      //    delimiters: ["((","))"]
+    })
 
 
-  axios.get('/movies.json')
-  .then(function (response) {
-    // handle success
-    console.log(response);
+    axios.get('/movies.json')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
   })
-})
