3190649b6b23e436329c8c45eb386c420ae87670
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-08 11:02:59 +0200

    Implemented: able to rate the movie without a page reload.
      To do: restrict this to logged-in users, show average rating.
    
     	modified:   app/javascript/packs/rate.js
     	new file:   app/views/movies/_index_footer.html.erb
     	modified:   app/views/movies/_index_header.html.erb
     	modified:   app/views/movies/index.html.erb
     	modified:   public/packs/manifest.json

--- app/javascript/packs/rate.js
+++ app/javascript/packs/rate.js
@@ -13,14 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
     data (){
 
       return {
-        rating : {
-          user_id: '',
-          movie_id: '',
-          stars: ''
-        },
+        rating : 0,
         current_user_id: 0,
         current_movie_id: 0,
-        score: 0, // current score
         movies_jsn:[],
         errors: []
       }
@@ -33,12 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     // https://forum.vuejs.org/t/select-element-by-id/21213/7
     mounted() {
-        this.current_user_id = this.$el.getAttribute('data-id');
-  //    current_user_id: document.getElementById("current-user");
-  //    console.log(current_user_id);
-//      console.log(this);
-  //    console.log(this.current_user_id.getAttribute('data-value'));
-    //  console.log("Info: ",current_user_id)
+      this.current_user_id = this.$el.getAttribute('data-id');
+      //    current_user_id: document.getElementById("current-user");
+      //    console.log(current_user_id);
+      //      console.log(this);
+      //    console.log(this.current_user_id.getAttribute('data-value'));
+      //  console.log("Info: ",current_user_id)
 
     },
     methods: {
@@ -47,53 +42,59 @@ document.addEventListener('DOMContentLoaded', () => {
         axios.get(url).then(response => {this.movies_jsn = response.data })
         .catch(e => {this.errors.push(e)})
 
-//      },
-//      removemovie(id) {
-//        let url=`http://localhost:3000/movies/` + id;
-//        console.log(url)
-//        this.axios.delete(url).then(response => {
-//          this.movies_json = this.movies_json.filter(movie => movie.id !== id)})
-//          .catch(e => {this.errors.push(e)});
-//          console.log(response)
+        //      },
+        //      removemovie(id) {
+        //        let url=`http://localhost:3000/movies/` + id;
+        //        console.log(url)
+        //        this.axios.delete(url).then(response => {
+        //          this.movies_json = this.movies_json.filter(movie => movie.id !== id)})
+        //          .catch(e => {this.errors.push(e)});
+        //          console.log(response)
       },
-        rate_movie: function (movie) {
-            // movie = current movie object
-            // console.log(this.current_user_id);
-            // console.log(movie.id);
-            // console.log(this.score);
-            console.log('user_id: '+this.current_user_id+', movie_id: '+movie.id, 'stars: '+this.score);
-//            axios.post('/ratings', { "user_id"=>"1","movie_id"=>"6","stars"=>1 })
-//                user_id: this.current_user_id,
-//                movie_id: movie.id,
-//                stars: this.score
-//              })
-//              .then(function (response) {
-//                console.log(response);
-//              })
-//              .catch(function (error) {
-//                console.log(error);
-//              });
+      rate_movie: function (movie) {
+        // movie = current movie object
+        // console.log(this.current_user_id);
+        // console.log(movie.id);
+        // console.log(this.score);
+        console.log('user_id: '+this.current_user_id+', movie_id: '+movie.id, 'stars: '+this.rating);
+        //            axios.post('/ratings', { "user_id"=>"1","movie_id"=>"6","stars"=>1 })
+        //                user_id: this.current_user_id,
+        //                movie_id: movie.id,
+        //                stars: this.score
+        //              })
+        //              .then(function (response) {
+        //                console.log(response);
+        //              })
+        //              .catch(function (error) {
+        //                console.log(error);
+        //              });
 
-//              axios.post('/ratings',{user_id:'1', movie_id: '5', stars: 5}).then(result => {
-              axios.post('/ratings',{user_id:this.current_user_id, movie_id: movie.id, stars: this.score}).then(result => {
+        //              axios.post('/ratings',{user_id:'1', movie_id: '5', stars: 5}).then(result => {
+        axios.post('/ratings',{user_id:this.current_user_id, movie_id: movie.id, stars: this.rating}).then(result => {
 
-                  alert("Movie rated!");
-                  console.log(result.data)
-            })
-          }
+          console.log(result.data)
+        })
 
-      },
-      // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
-      components: { Rate }
-      //    ,
-      // https://stackoverflow.com/questions/44440708/vue-js-templating-and-interpolation-not-rendering-data
-      //    delimiters: ["((","))"]
-    })
+        if (this.rating>1) {
+          alert("You gave '"+movie.title+"' "+this.rating+" stars.")
+        } else {
+          alert("You rated '"+movie.title+"' just one star.")
+        }
+        this.rating=0; // Important: reset number of stars to zero (otherwise they would be appear filled in all rows)
+      }
 
-
-    axios.get('/movies.json')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
+    },
+    // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
+    components: { Rate }
+    //    ,
+    // https://stackoverflow.com/questions/44440708/vue-js-templating-and-interpolation-not-rendering-data
+    //    delimiters: ["((","))"]
   })
+
+
+  axios.get('/movies.json')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+})
