565f621cb2844ef548faa1ade2af08c75b30c555
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-11 12:20:10 +0200

    Linted script file
     	modified:   app/javascript/packs/facet.js
    
      new file:   .eslintrc.yml
    
     	deleted:    app/javascript/packs/rate.js
      deleted:    app/javascript/rate.vue
    
      (Removed earlier version, called 'rate', used during development)
    
     	modified:   package.json
     	modified:   public/packs/manifest.json
     	modified:   yarn.lock

--- app/javascript/packs/facet.js
+++ app/javascript/packs/facet.js
@@ -13,7 +13,7 @@ import 'vue-select/dist/vue-select.css';
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#facet',
-    data (){
+    data() {
 
       return {
         all_label: '--- All ---',
@@ -23,129 +23,68 @@ document.addEventListener('DOMContentLoaded', () => {
         selected: '',
         selected_category: '',
         selected_rating: '',
-        options: ['First','Second','Third'],
-        rating : 0,
+        options: ['First', 'Second', 'Third'],
+        rating: 0,
         current_user_id: 0,
         current_movie_id: 0,
-        movies_jsn:[],
+        movies_jsn: [],
         errors: [],
         current_page: 1,
         records_per_page: 3,
         // https://www.raymondcamden.com/2018/02/08/building-table-sorting-and-pagination-in-vuejs
-        pageSize:5,
-        currentPage:1
+        pageSize: 5,
+        currentPage: 1
 
       }
 
     },
     computed: {
 
-      filteredMovieIds: function() {
-
-        let ids=[];
-
-        console.log(app.filteredMovies.ids);
-
-        this.filteredMovies.forEach(function(movie) {
-          ids.push(movie.id)
-        });
-
-        return ids;
-
-      },
-
-      paginatedMovies: function() {
-
-        return filteredMovies.slice(app.start_indx, app.finish_indx);
-
-      },
-
-      start_indx: function() {
-
-         return (this.current_page-1)*(this.records_per_page);
-     },
-
-     finish_indx: function() {
-
-        return app.start_indx+this.records_per_page;
-    },
-
-
-        totalPages: function() {
-
-          return Math.ceil(app.filteredMovies.length/this.records_per_page);
-
-          return app.filteredMovies.length/(app.pagination.max_records_per_page);
-      },
-
-       paginatedMovies: function() {
-
-//         let start_indx=(this.current_page-1)*(this.records_per_page);
-//          let finish_indx=start_indx+this.records_per_page;
-//         return app.filteredMovies.slice(start_indx,finish_indx);
-      },
-
-      filteredTitles: function() {
-
-        let titles=[];
-
-        this.filteredMovies.forEach(function(movie) {
-          titles.push(movie.title)
-        });
-
-        return titles;
-
-      },
       filteredMovies: function() {
 
-        // This would search by movie title as well
+        // This would search by movie title as well (for pagination, add filter below)
         // return app.movieTitleContainsText(app.movieSummaryContainsText(app.moviesByCategory(app.moviesByRating(this.movies_jsn))));
         // <p>Search: <input v-model="search_title_txt" placeholder="Movie titles"></p>
 
-//        return app.movieTitleContainsText(app.movieSummaryContainsText(app.moviesByCategory(app.moviesByRating(this.movies_jsn))));
+        //        return app.movieTitleContainsText(app.movieSummaryContainsText(app.moviesByCategory(app.moviesByRating(this.movies_jsn))));
 
-        return app.movieTitleContainsText(app.movieSummaryContainsText(app.moviesByCategory(app.moviesByRating(this.movies_jsn)))).filter((row, index) => {
-		let start = (this.currentPage-1)*this.pageSize;
-		let end = this.currentPage*this.pageSize;
-		if(index >= start && index < end) return true;
-	});
+        return app.movieTitleContainsText(app.movieSummaryContainsText(app.moviesByCategory(app.moviesByRating(this.movies_jsn)))).
+        // filter added for pagination, as in: https://www.raymondcamden.com/2018/02/08/building-table-sorting-and-pagination-in-vuejs
+        filter((row, index) => {
+          let start = (this.currentPage - 1) * this.pageSize;
+          let end = this.currentPage * this.pageSize;
+          if (index >= start && index < end) return true;
+        });
 
-        },
+      },
       // a computed getter
-      category_names: function () {
+      category_names: function() {
         // `this` points to the vm instance
-        console.log(this.movies_jsn.category);
-        let category_names=[];
-        for (var i=0; i<this.movies_jsn.length; i++) {
+        let category_names = [];
+        for (var i = 0; i < this.movies_jsn.length; i++) {
           //let name=JSON.stringify(this.movies_jsn[i].category.name)
-          let name=this.movies_jsn[i].category.name
+          let name = this.movies_jsn[i].category.name
 
-          console.log(name);
-          if(category_names.includes(name) === false) { category_names.push(name); }
+          if (category_names.includes(name) === false) {
+            category_names.push(name);
+          }
 
-
-          //        category_names.push(cat_name);
         }
-
-        console.log(category_names.sort());
-
-        //  console.log(category_names)
-
 
         return category_names.sort();
 
 
       },
-      category_name_options: function () {
-        let arr=[];
+      category_name_options: function() {
+        let arr = [];
         arr.push(this.all_label);
         arr.push(this.category_names);
         return arr.flat();
       },
-      rating_options: function () {
-        let arr=[];
+      rating_options: function() {
+        let arr = [];
         arr.push(this.all_label);
-        let levels=['5','4','3','2','1'];
+        let levels = ['5', '4', '3', '2', '1'];
         arr.push(levels);
         return arr.flat();
       },
@@ -153,57 +92,40 @@ document.addEventListener('DOMContentLoaded', () => {
     // https://alligator.io/vuejs/rest-api-axios/
     // https://stackoverflow.com/questions/54757510/how-to-delete-a-record-in-rails-api-and-vue-js
     created() {
-      //      console.log('User id ===> ');
       // https://forum.vuejs.org/t/how-to-get-id-value-in-vue-js/44999
-      let user_id=document.querySelector('#current_user').dataset.value;
+      let user_id = document.querySelector('#current_user').dataset.value;
       //      console.log(user_id);
-      this.current_user_id=user_id;
+      this.current_user_id = user_id;
       this.getmovies();
     },
     // https://forum.vuejs.org/t/select-element-by-id/21213/7
     mounted() {
 
-      //      this.current_user_id = app.getAttribute('data-current-user-id');
-      //    current_user_id: document.getElementById("current-user");
-      //    console.log(current_user_id);
-      //      console.log(this);
-      //    console.log(this.current_user_id.getAttribute('data-value'));
-      //  console.log("Info: ",current_user_id)
-
     },
     methods: {
 
       // https://www.raymondcamden.com/2018/02/08/building-table-sorting-and-pagination-in-vuejs
-      nextPage:function() {
-        if((this.currentPage*this.pageSize) < this.movies_jsn.length) this.currentPage++;
+      nextPage: function() {
+        if ((this.currentPage * this.pageSize) < this.movies_jsn.length) this.currentPage++;
       },
-      prevPage:function() {
-        if(this.currentPage > 1) this.currentPage--;
+      prevPage: function() {
+        if (this.currentPage > 1) this.currentPage--;
       },
 
       movieTitleContainsText(movies) {
 
         return movies.filter((movie) => {
 
-          if (this.search_title_txt!='')
-          {
+          if (this.search_title_txt != '') {
 
-            console.log(this.search_tile_txt);
-            console.log(movie.title);
+            let movie_text = movie.title.toUpperCase();
+            let search_title_txt_case_insensitive = this.search_title_txt.toUpperCase();
 
-            let movie_text=movie.title.toUpperCase();
-            let search_title_txt_case_insensitive=this.search_title_txt.toUpperCase();
-            // insert logic here
-            return   movie_text.match(search_title_txt_case_insensitive);
-
-            //      let selected_rating=this.selected_rating;
-            //      let movie_average=app.average_rating(movie);
-            //    return  ((movie_average>=selected_rating) && (movie_average-selected_rating<=1));
+            return movie_text.match(search_title_txt_case_insensitive);
 
           } else {
             return true;
           }
-          this.search_title_txt=''; // reset
 
         });
 
@@ -213,25 +135,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return movies.filter((movie) => {
 
-          if (this.search_summary_txt!='')
-          {
+          if (this.search_summary_txt != '') {
 
-            console.log(this.search_summary_txt);
-            console.log(movie.summary);
-
-            let movie_text=movie.summary.toUpperCase();
-            let search_summary_txt_case_insensitive=this.search_summary_txt.toUpperCase();
-            // insert logic here
-            return   movie_text.match(search_summary_txt_case_insensitive);
-
-            //      let selected_rating=this.selected_rating;
-            //      let movie_average=app.average_rating(movie);
-            //    return  ((movie_average>=selected_rating) && (movie_average-selected_rating<=1));
+            let movie_text = movie.summary.toUpperCase();
+            let search_summary_txt_case_insensitive = this.search_summary_txt.toUpperCase();
+            return movie_text.match(search_summary_txt_case_insensitive);
 
           } else {
             return true;
           }
-          this.search_summary_txt=''; // reset
 
         });
 
@@ -242,12 +154,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return movies.filter((movie) => {
 
-          if ((this.selected_rating!='') && (this.selected_rating!=this.all_label))
-          {
+          if ((this.selected_rating != '') && (this.selected_rating != this.all_label)) {
 
-            let selected_rating=this.selected_rating;
-            let movie_average=app.average_rating(movie);
-            return  ((movie_average>=selected_rating) && (movie_average-selected_rating<=1));
+            let selected_rating = this.selected_rating;
+            let movie_average = app.average_rating(movie);
+            return ((movie_average >= selected_rating) && (movie_average - selected_rating <= 1));
 
           } else {
             return true;
@@ -260,10 +171,9 @@ document.addEventListener('DOMContentLoaded', () => {
         return movies.filter((movie) => {
           //          console.log(movie.category.name);
 
-          if ((this.selected_category!='') && (this.selected_category!=this.all_label))
-          {
+          if ((this.selected_category != '') && (this.selected_category != this.all_label)) {
 
-            return movie.category.name==this.selected_category;
+            return movie.category.name == this.selected_category;
           } else {
             return true;
           }
@@ -273,20 +183,12 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       // get the average rate (number of stars)
-      number_of_ratings(movie) {
-        console.log(movie.title);
-        return -1;
-        //        return movie.ratings.length;
-
-      },
       average_rating(movie) {
         if (movie.ratings.length > 0) {
 
-          let score=app.total_stars(movie)/app.number_of_ratings(movie);
-          return Math.round(score * 100)/100;
-        }
-
-        else
+          let score = app.total_stars(movie) / app.number_of_ratings(movie);
+          return Math.round(score * 100) / 100;
+        } else
         return 0;
 
         //        return total_stars(movie)/number_of_ratings(movie) ;
@@ -298,22 +200,24 @@ document.addEventListener('DOMContentLoaded', () => {
         return movie.ratings.length;
       },
       total_stars(movie) {
-        let movie_ratings=movie.ratings;
-        let total_stars=0;
+        let movie_ratings = movie.ratings;
+        let total_stars = 0;
         movie_ratings.forEach(function(movie_rating) {
-          total_stars+=movie_rating.stars;
+          total_stars += movie_rating.stars;
         });
-        console.log(movie.title+" has "+total_stars+" stars in total.")
         return total_stars;
-        //        return movie.ratings.length;
       },
-      getmovies () {
-        let url=`/movies.json`;
-        axios.get(url).then(response => {this.movies_jsn = response.data })
-        .catch(e => {this.errors.push(e)})
+      getmovies() {
+        let url = `/movies.json`;
+        axios.get(url).then(response => {
+          this.movies_jsn = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
 
       },
-//        remove_movie(id) {
+      //        remove_movie(id) {
       //        let url=`http://localhost:3000/movies/` + id;
       //        console.log(url)
       //        this.axios.delete(url).then(response => {
@@ -321,52 +225,40 @@ document.addEventListener('DOMContentLoaded', () => {
       //          .catch(e => {this.errors.push(e)});
       //          console.log(response)
 
-      rate_movie: function (movie) {
-        // movie = current movie object
-        // console.log(this.current_user_id);
-        // console.log(movie.id);
-        // console.log(this.score);
-        console.log('user_id: '+this.current_user_id+', movie_id: '+movie.id, 'stars: '+this.rating);
-        //            axios.post('/ratings', { "user_id"=>"1","movie_id"=>"6","stars"=>1 })
-        //                user_id: this.current_user_id,
-        //                movie_id: movie.id,
-        //                stars: this.score
-        //              })
-        //              .then(function (response) {
-        //                console.log(response);
-        //              })
-        //              .catch(function (error) {
-        //                console.log(error);
-        //              });
+      rate_movie: function(movie) {
 
-        //              axios.post('/ratings',{user_id:'1', movie_id: '5', stars: 5}).then(result => {
-        axios.post('/ratings',{user_id:this.current_user_id, movie_id: movie.id, stars: this.rating}).then(result => {
-
-          console.log(result.data)
+        axios.post('/ratings', {
+          user_id: this.current_user_id,
+          movie_id: movie.id,
+          stars: this.rating
         })
+        //    }).then(result => {
 
-        if (this.rating>1) {
-          alert("You gave '"+movie.title+"' "+this.rating+" stars.")
+        //        console.log(result.data)
+        //      })
+
+        //               .catch(function (error) {
+        //                 console.log(error);
+        //                });
+
+        if (this.rating > 1) {
+          alert("You gave '" + movie.title + "' " + this.rating + " stars.")
         } else {
-          alert("You rated '"+movie.title+"' just one star.")
+          alert("You rated '" + movie.title + "' just one star.")
         }
-        this.rating=0; // Important: reset number of stars to zero (otherwise they would be appear filled in all rows)
+        this.rating = 0; // Important: reset number of stars to zero (otherwise they would be appear filled in all rows)
       }
 
     },
     // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
-    components: { }
-    //    ,
-    // https://stackoverflow.com/questions/44440708/vue-js-templating-and-interpolation-not-rendering-data
-    //    delimiters: ["((","))"]
+    components: {}
+
   })
 
-  //
-
-  axios.get('/movies.json')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
+  axios.get('/movies.json');
+  //    .then(function(response) {
+  // handle success
+  //      console.log(response);
+  //    })
 
 })
