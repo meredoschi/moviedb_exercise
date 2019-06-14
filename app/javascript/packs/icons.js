b59762268d7e76a04ea2ffec60727a96577e3194
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-14 07:38:29 +0200

    Added star icon to the movie show view
      https://github.com/antonreshetov/vue-eva-icons
    
     	new file:   app/javascript/icons.vue
     	new file:   app/javascript/packs/icons.js
     	modified:   app/views/movies/show.html.erb
     	modified:   package.json
     	modified:   public/packs/manifest.json
     	modified:   yarn.lock

---
+++ app/javascript/packs/icons.js
@@ -0,0 +1,36 @@
+import Vue from 'vue/dist/vue.esm';
+
+import EvaIcons from 'vue-eva-icons'
+Vue.use(EvaIcons)
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        el: '#icons',
+        data() {
+
+            return {
+            }
+
+        },
+        computed: {
+
+        },
+
+        created() {
+
+        },
+
+        mounted() {
+
+        },
+        methods: {
+
+        },
+        // https://stackoverflow.com/questions/50169210/vuejs-redirect-to-url-when-clicked-on-a-button
+        components: {}
+
+    })
+
+})
