392f0aadcf232ab29c02977a490294d481c4e914
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-17 19:42:30 +0200

    A member may not create two movies with the same title
      	new file:   db/migrate/20190618160027_add_index_to_movies.rb
      	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_155140) do
+ActiveRecord::Schema.define(version: 2019_06_18_160027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_155140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["title", "user_id"], name: "index_movies_on_title_and_user_id", unique: true
   end
 
   create_table "ratings", force: :cascade do |t|
