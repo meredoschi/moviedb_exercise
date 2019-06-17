1de79324059e6e4a85090a6b185b5e6f59432daf
Marcelo Eduardo Redoschi <mredoschi@uol.com.br>
2019-06-17 19:42:30 +0200

    Started to add functionalities to the template (from facet.js)
      Movie table is rendered (on the development environment)
    
     	modified:   app/javascript/components/development/movies.vue
      modified:   app/views/movies/index.html.erb
    
     	new file:   public/packs/js/development/movies-7eb680ee14185254cdfd.js
     	new file:   public/packs/js/development/movies-7eb680ee14185254cdfd.js.map
     	new file:   public/packs/js/facet-572c3b1fe929826b151d.js
     	new file:   public/packs/js/facet-572c3b1fe929826b151d.js.map
     	new file:   public/packs/js/icons-ad038ce1457eadb7ec16.js
     	new file:   public/packs/js/icons-ad038ce1457eadb7ec16.js.map
     	modified:   public/packs/manifest.json

--- app/javascript/components/development/movies.vue
+++ app/javascript/components/development/movies.vue
@@ -1,9 +1,104 @@
 <template>
-  <div id="app-movie-list">
+  <div id="app-movies">
 
-    {{ currentPage }}
+    <!--
 
-    User: {{ current_user_id }}
+    <table v-if="(filteredMovies.length>0)" class="table table-striped" id="movies-table">
+
+    <table class="table table-striped" id="movies-table">
+
+  -->
+
+    <table class="table table-striped" id="movies-table">
+
+
+      <thead class="thead-dark">
+        <tr>
+          <th>Title</th>
+          <th>Text</th>
+          <th>Ratings</th>
+
+        <!--
+        <th v-if="user_logged_in"></th>
+      -->
+          <th></th>
+          <th>Category</th>
+
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+
+        <tbody>
+          <template v-for="(movie, index) in movies_jsn">
+
+            <tr>
+              <td><a v-bind:id="`title-${movie.id}`" v-bind:href="`movies/${movie.id}`">{{ movie.title }}</a></td>
+              <td><a v-bind:id="`summary-${movie.id}`" v-bind:href="`movies/${movie.id}`">{{ movie.summary }}</a></td>
+
+              <td>
+              <!--
+
+              {{ average_rating(movie)}}
+            -->
+
+                <eva-icon name="star" fill="#05e5d6" animation="pulse" height="22">
+                </eva-icon>
+
+              </td>
+
+              <td>
+
+            <!--
+
+            <template v-if="(rating_stars_visible(movie)) ">
+
+            <star-rating v-model="rating" v-bind:star-size="18" active-color="#ebf442" @rating-selected ="rate_movie(movie)"></star-rating>
+
+          </template>
+
+             -->
+
+             </td>
+
+             <td>{{ movie.category["name"] }}</td>
+
+             <td>
+               <a v-bind:id="`show-${movie.id}`" v-bind:href="`movies/${movie.id}`" >
+               <button type="button" class="btn btn-primary">Show
+               </button>
+               </a>
+             </td>
+
+
+             <td>
+
+        <!--
+
+        <a v-if="owner(movie)" v-bind:id="`edit-${movie.id}`" v-bind:href="`movies/${movie.id}/edit`" >
+        <button type="button" class="btn btn-warning">Edit
+      </button>
+    </a>
+
+    -->
+
+            </td>
+
+            <td>
+
+      <!--
+
+      <td v-on:click="invoke_destroy(movie)" v-if="owner(movie)" v-bind:id="`destroy-${movie.id}`"><button type="button" class="btn btn-danger">Remove</button></td>
+
+    -->
+
+            </td>
+
+          </tr>
+
+        </template>     <!-- v-for loop end  -->
+      </tbody>
+    </table>
+
   </div>
 </template>
 
@@ -37,19 +132,35 @@ export default {
       // https://www.raymondcamden.com/2018/02/08/building-table-sorting-and-pagination-in-vuejs
       pageSize: 10,
       currentPage: 1,
+      movies_jsn: [],
       //
       movie_ids_recently_rated: []
     }
 
 
   },
+  methods: {
+
+
+    getmovies() {
+      let url = `/movies.json`;
+      axios.get(url).then(response => {
+        this.movies_jsn = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+
+    }
+
+  },
   // https://alligator.io/vuejs/rest-api-axios/
   // https://stackoverflow.com/questions/54757510/how-to-delete-a-record-in-rails-api-and-vue-js
   created() {
-      // https://forum.vuejs.org/t/how-to-get-id-value-in-vue-js/44999
-      let user_id = document.querySelector('#current_user').dataset.value;
-      this.current_user_id = user_id;
-    //  this.getmovies();
+    // https://forum.vuejs.org/t/how-to-get-id-value-in-vue-js/44999
+    let user_id = document.querySelector('#current_user').dataset.value;
+    this.current_user_id = user_id;
+    this.getmovies();
   },
   components: {
 
